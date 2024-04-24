12fcdc825ca498fda841702d57e4f28dafecf69a
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-04-24 16:36:35 +0300

    custom hook for feedback items

---
+++ src/contexts/FeedbackItemsContextProvider.tsx
@@ -0,0 +1,115 @@
+import { createContext, useEffect, useMemo, useState } from 'react';
+import { TFeedbackItem } from '../lib/types';
+
+type TFeedbackItemsContext = {
+  filteredFeedbackItems: TFeedbackItem[];
+  isLoading: boolean;
+  errorMessage: string;
+  companyList: string[];
+  handleAddToList: (text: string) => Promise<void>;
+  handleSelectCompany: (company: string) => void;
+};
+
+type FeedbackItemsContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export const FeedbackItemsContext = createContext<TFeedbackItemsContext | null>(
+  null
+);
+
+export default function FeedbackItemsContextProvider({
+  children,
+}: FeedbackItemsContextProviderProps) {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [selectedCompany, setSelectedCompany] = useState('');
+
+  const companyList = useMemo(
+    () =>
+      feedbackItems
+        .map((item) => item.company)
+        .filter((company, index, array) => array.indexOf(company) === index),
+    [feedbackItems]
+  );
+
+  const filteredFeedbackItems = useMemo(
+    () =>
+      selectedCompany
+        ? feedbackItems.filter(
+            (feedbackItem) => feedbackItem.company === selectedCompany
+          )
+        : feedbackItems,
+    [feedbackItems, selectedCompany]
+  );
+
+  const handleAddToList = async (text: string) => {
+    const companyName = text
+      .split(' ')
+      .find((word) => word.includes('#'))!
+      .substring(1);
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      text: text,
+      upvoteCount: 0,
+      daysAgo: 0,
+      company: companyName,
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+    };
+    setFeedbackItems([...feedbackItems, newItem]);
+
+    await fetch(
+      'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks',
+      {
+        method: 'POST',
+        body: JSON.stringify(newItem),
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'application/json',
+        },
+      }
+    );
+  };
+
+  const handleSelectCompany = (company: string) => {
+    setSelectedCompany(company);
+  };
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
+        );
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage('Something went wrong. Please try again later.');
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+
+  return (
+    <FeedbackItemsContext.Provider
+      value={{
+        filteredFeedbackItems,
+        isLoading,
+        errorMessage,
+        companyList,
+        handleAddToList,
+        handleSelectCompany,
+      }}
+    >
+      {children}
+    </FeedbackItemsContext.Provider>
+  );
+}
